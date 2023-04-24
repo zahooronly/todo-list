4b009e498226260df5068ffbf408fb5f97ea062c
Mr. Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-04-24 12:20:43 +0500

    finally solve type erros, but don't know hwo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
+// import { suppressHydrationWarning } from "re act-dom/server";
 import Todo from "@/component/Todo";
 import { AiOutlinePlus } from "react-icons/ai";
 import { db } from "@/component/Config/firebase.config";
@@ -8,8 +9,13 @@ import {
   collection,
   onSnapshot,
   query,
+  updateDoc,
+  doc,
+  addDoc,
 } from "firebase/firestore";
-
+// if (typeof window === "undefined") {
+//   suppressHydrationWarning(true);
+// }
 const styles = {
   bg: `min-h-screen w-screen p-4 bg-gradient-to-r from-gray-600 to-gray-800`,
   heading: `text-4xl font-bold text-center text-gray-700 p-3`,
@@ -19,7 +25,7 @@ const styles = {
   input: `border outline-gray-900/70 w-full text-xl p-3 rounded-[20px]`,
   count: `text-center p-4 text-bold text-xl`,
   li: `flex justify-between p-3 my-2`,
-  liCompleted: `flex justify-between bg-slate-200 capitalize `,
+  liCompleted: `flex justify-between bg-slate-200 text-green-600 capitalize`,
   row: `flex`,
   text: `ml-2 cursor-pointer text-xl`,
   ul: ``,
@@ -28,19 +34,11 @@ const styles = {
 
 const Home = () => {
   const [todo, setTodo] = useState<string[]>([]);
+  const [input, setInput] = useState("");
 
-  // useEffect(() => {
-  //   const q = query(collection(db, "todos"));
-  //   const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
-  //     let todoList: [] = [];
-  //     QuerySnapshot.forEach((doc) => {
-  //       todoList.push({ ...doc.data(), id: doc.id });
-  //     });
-  //     setTodo(todoList);
-  //   });
-  //   return () => unsubscribe();
-  // }, []);
-
+  // Create Data      *C*
+  // const createTodo = async () => {};
+  // Read Data      *R*
   useEffect(() => {
     const q = query(collection(db, "todos"));
     const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
@@ -53,17 +51,38 @@ const Home = () => {
     return () => unsubscribe();
   }, []);
 
-  const todoAdder = (event: React.FormEvent<HTMLFormElement>) => {
+  // Updata Data    *U*
+  // const toggle = async (todo: any) => {
+  //   await updateDoc(doc(db, "todos", todo.id), {
+  //     completed: !todo.completed,
+  //   });
+  // };
+  // Props;
+
+  // const toggle = async (todo: { id: string; completed: boolean }) => {
+  const toggle: any = async (todo: any) => {
+    await updateDoc(doc(db, "todos", todo.id), {
+      completed: !todo.completed,
+    });
+  };
+
+  const todoAdder = async (event: any) => {
     event.preventDefault();
 
     const formData = new FormData(event.currentTarget);
     const data = formData.get("task") as string;
     if (!data) {
       alert("Please write some text first");
+      return;
     } else {
-      setTodo([...todo, data]);
+      // setTodo([...todo, data]);
+      await addDoc(collection(db, "todos"), {
+        text: input,
+        completed: false,
+      });
+      // setInput(event.target.reset());
+      setInput("");
     }
-    event.currentTarget.reset();
   };
 
   return (
@@ -78,6 +97,10 @@ const Home = () => {
             type="text"
             className={styles.input}
             name="task"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
             placeholder="Enter your task..."
           />
           <button className={styles.button} type="submit">
@@ -86,7 +109,13 @@ const Home = () => {
         </form>
         <ul className={styles.ul}>
           {todo.map((todo, index) => (
-            <Todo key={index} todo={todo} />
+            // <Todo key={index} todo={todo} toggle={toggle} />
+            // <Todo
+            //   key={index}
+            //   todo={{ id: todo.id, text: todo.text, completed: todo.completed }}
+            //   toggle={toggle}
+            // />
+            <Todo key={index} todo={todo} toggle={toggle} />
           ))}
         </ul>
         <p className={styles.count}>You have {todo.length} todos</p>
--- component/Todo.tsx
+++ component/Todo.tsx
@@ -1,33 +1,68 @@
-import React from "react";
-import { FaRegTrashAlt } from "react-icons/fa";
-interface Props {
-  todo: string;
-}
+// import React from "react";
+// import { FaRegTrashAlt } from "react-icons/fa";
+// // interface Props {
+// //   todo: string;
+// // }
 
-const style = {
+const styles = {
   // ul:`p-3`,
   li: `flex justify-between p-3 my-2 bg-gray-500/90`,
   liCompleted: `flex justify-between bg-slate-200 capitalize `,
   row: `flex`,
   textStyle: `ml-2 cursor-pointer text-md`,
   checkbox: `valid:border-green-500`,
-  textCompleted: `ml-2 cursor-pointer line-through text-xl`,
+  textCompleted: `ml-2 cursor-pointer line-through text-green-600 text-md`,
 };
 
-const Todo: React.FC<Props> = ({ todo }: any) => {
+// const Todo: React.FC<Props> = ({ todo, toggle }: any) => {
+//   return (
+//     <ul>
+//       <li className={todo.copleted ? style.liCompleted : style.li}>
+//         <div className={style.row}>
+//           <input
+//             onChange={() => toggle(todo)}
+//             className="valid:border-green-500 h-5 w-5 text-green-600"
+//             type="checkbox"
+//             checked={todo.completed ? true : false}
+//           />
+
+//           <p
+//             onClick={() => toggle(todo)}
+//             className={todo.completed ? style.textCompleted : style.textStyle}
+//           >
+//             {todo.text}
+//           </p>
+//         </div>
+//         <FaRegTrashAlt />
+//       </li>
+//     </ul>
+//   );
+// };
+
+// export default Todo;
+
+import React from "react";
+
+// interface Props {
+//   key: number;
+//   todo: {
+//     id: string;
+//     text: string;
+//     completed: boolean;
+//   };
+//   toggle: (todo: { id: string; completed: boolean }) => Promise<void>;
+// }
+
+const Todo = ({ todo, toggle }: any) => {
   return (
-    <ul>
-      <li className={style.li}>
-        <div className={style.row}>
-          <input
-            className="valid:border-green-500 h-5 w-5 text-green-600"
-            type="checkbox"
-          />
-          <p className={style.textStyle}>{todo.text}</p>
-        </div>
-        <FaRegTrashAlt />
-      </li>
-    </ul>
+    <li
+      onClick={() => toggle(todo)}
+      className={`${todo.completed ? styles.liCompleted : styles.li}`}
+    >
+      <div className={styles.row}>
+        <div className={styles.textStyle}>{todo.text}</div>
+      </div>
+    </li>
   );
 };
 
